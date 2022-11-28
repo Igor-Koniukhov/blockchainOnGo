4d65b3cf9d3b34886e8906ce28659fb7dedb7be4
Igor-Koniukhov <ikoniukhov@gmail.com>
2022-11-28 17:52:51 +0200

    chainged structure of project

--- block/blockchain.go
+++ block/blockchain.go
@@ -4,16 +4,21 @@ import (
 	"crypto/sha256"
 	"encoding/json"
 	"fmt"
+	"log"
 	"strings"
 	"time"
 )
 
-const MINING_DIFFICULTY = 3
+const (
+	MINING_DIFFICULTY = 3
+	MINING_SENDER     = "THE BLOCKCHAIN"
+	MINING_REWARD     = 1.0
+)
 
 type Block struct {
+	timestamp    int64
 	nonce        int
 	previousHash [32]byte
-	timestamp    int64
 	transactions []*Transaction
 }
 
@@ -55,13 +60,15 @@ func (b *Block) MarshalJSON() ([]byte, error) {
 }
 
 type Blockchain struct {
-	transactionPool []*Transaction
-	chain           []*Block
+	transactionPool   []*Transaction
+	chain             []*Block
+	blockchainAddress string
 }
 
-func NewBlockchain() *Blockchain {
+func NewBlockchain(blockchainAddress string) *Blockchain {
 	b := &Block{}
 	bc := new(Blockchain)
+	bc.blockchainAddress = blockchainAddress
 	bc.CreateBlock(0, b.Hash())
 	return bc
 }
@@ -84,20 +91,6 @@ func (bc *Blockchain) LastBlock() *Block {
 	return bc.chain[len(bc.chain)-1]
 }
 
-func main() {
-	blockChain := NewBlockchain()
-	blockChain.Print()
-
-	blockChain.AddTransaction("A", "B", 1.0)
-
-	previousHash := blockChain.LastBlock().Hash()
-	blockChain.CreateBlock(3, previousHash)
-	blockChain.Print()
-
-	previousHash = blockChain.LastBlock().Hash()
-	blockChain.CreateBlock(4, previousHash)
-	blockChain.Print()
-}
 func (bc *Blockchain) AddTransaction(sender string, recipient string, value float32) {
 	t := NewTransaction(sender, recipient, value)
 	bc.transactionPool = append(bc.transactionPool, t)
@@ -113,6 +106,50 @@ func (bc *Blockchain) CopyTransactionPool() []*Transaction {
 				t.value))
 	}
 	return transactions
+}
+
+func (bc *Blockchain) ValidProof(nonce int, previousHash [32]byte, transactions []*Transaction, difficulty int) bool {
+	zeros := strings.Repeat("0", difficulty)
+	guessBlock := Block{0, nonce, previousHash, transactions}
+	guessHashStr := fmt.Sprintf("%x", guessBlock.Hash())
+	fmt.Println(guessHashStr)
+
+	return guessHashStr[:difficulty] == zeros
+
+}
+
+func (bc *Blockchain) ProofOfWork() int {
+	transactions := bc.CopyTransactionPool()
+	previousHash := bc.LastBlock().Hash()
+	nonce := 0
+	for !bc.ValidProof(nonce, previousHash, transactions, MINING_DIFFICULTY) {
+		nonce += 1
+	}
+	return nonce
+}
+func (bc *Blockchain) Mining() bool {
+	bc.AddTransaction(MINING_SENDER, bc.blockchainAddress, MINING_REWARD)
+	nonce := bc.ProofOfWork()
+	previousHash := bc.LastBlock().Hash()
+	bc.CreateBlock(nonce, previousHash)
+	log.Println("action=mining, status=success")
+	return true
+}
+func (bc *Blockchain) CalculateTotalAmount(blockChainAddress string) float32 {
+	var totalAmount float32 = 0.0
+	for _, b := range bc.chain {
+		for _, t := range b.transactions {
+			value := t.value
+			if blockChainAddress == t.recipientBlockchainAddress {
+				totalAmount += value
+			}
+			if blockChainAddress == t.senderBlockchainAddress {
+				totalAmount -= value
+			}
+
+		}
+	}
+	return totalAmount
 }
 
 type Transaction struct {
